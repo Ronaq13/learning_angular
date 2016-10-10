517ed4d02c2c12379d91c6347530bf1c608caeb1
Ronaq13 <raounaqsharma@gmail.com>
2016-10-11 01:08:37 +0530

    [MAJOR] ng-repeat + filter

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ var app = angular.module('myApp', []);
 
 // 1st controller
 app.controller('myCtrl', function($scope, $http) {
-    // $scope.message = "I got it"; //always displayed if in view you called 'message'
-    $scope.heading = "GitHub Viewer";
-    $scope.username = ""; //default username
-    var onUserComplete = function(obj) { //you can give any name to this 1st parameter of function, as it recieve a JS object from JSON file so we are just using this with any name and then coppying the same object into another object named user.
-        $scope.user = obj.data;
-    };
-    var onerror = function() {
-        $scope.errorMessage = "Not able to find this"; //only displayed if function is called
-    };
 
-    //  $http.get("https://api.github.com/users/Ronaq13")
-    //    .then(onUserComplete, onerror); //if onUserComplete doesnt get called the onerror will be called
+    $scope.heading = "GitHub Viewer";
+    $scope.username = ""; //default username ->nothing
 
 
     $scope.search = function(username) {
         $http.get("https://api.github.com/users/" + username)
-            .then(onUserComplete, onerror); //if onUserComplete doesnt get called the onerror will be called
+            .then(onUserComplete, onError); //if onUserComplete doesnt get called the onerror will be called
+    };
+
+    var onUserComplete = function(obj) { //you can give any name to this 1st parameter of function, as it recieve a JS object from JSON file so we are just using this with any name and then coppying the same object into another object named user.
+        $scope.user = obj.data;
+        $http.get($scope.user.repos_url)
+            .then(onRepos, onError);
+    };
+
+    var onRepos = function(response) { // response : the JS object you get from user.repos_url
+        $scope.repos = response.data; //this will send the 'complete array of repositories' under the name repos
+    };
+
+    var onError = function() {
+        $scope.errorMessage = "Not able to find this"; //only displayed if function is called
     };
 
 });
