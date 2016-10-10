53aaac85c4f469c814627cf18dbe35b6fae70bb7
Ronaq13 <raounaqsharma@gmail.com>
2016-10-10 23:09:51 +0530

    $http module with a error function also.

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ var app = angular.module('myApp', []);
 
 // 1st controller
 app.controller('myCtrl', function($scope, $http) {
-
-    var onUserComplete = function(response) {
-        $scope.user = response.data;
+    $scope.message = "I got it";
+    var onUserComplete = function(obj) {
+        $scope.user = obj.data;
+    };
+    var onerror = function() {
+        $scope.message = "Could not connect with the server";
     };
 
     $http.get("https://api.github.com/users/Ronaq13")
-        .then(onUserComplete);
+        .then(onUserComplete, onerror); //if onUserComplete doesnt get called the onerror will be called
 });
